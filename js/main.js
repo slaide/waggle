15416d4476bb2565933b1279ea6ee0d23af0ffb4
Patrick Hennig <patrick_hennig@fastmail.com>
2025-04-17 16:04:30 +0200

    implement texture sampling

--- js/main.js
+++ js/main.js
@@ -66,6 +66,183 @@ function createShaderProgram(gl,stageSources){
     }
 }
 
+/**
+ * @typedef {{
+ *     program: WebGLProgram,
+ *     attribLocations: {
+ *         vertexPosition: GLint,
+ *         vertexUVCoords:GLint,
+ *     },
+ *     uniformLocations: {
+ *         projectionMatrix: WebGLUniformLocation,
+ *         modelViewMatrix: WebGLUniformLocation,
+ *         uSampler: WebGLUniformLocation,
+ *     }
+ * }} ProgramInfo
+* */
+/**
+ * 
+ * @param {WebGL2RenderingContext} gl
+ * @returns {ProgramInfo}
+ */
+function makeProgram(gl){
+    const shaderProgram=createShaderProgram(gl,{
+        vs:`
+            attribute vec4 aVertexPosition;
+            attribute vec2 aVertexTexCoord;
+
+            uniform mat4 uModelViewMatrix;
+            uniform mat4 uProjectionMatrix;
+
+            varying highp vec2 vTextureCoord;
+
+            void main() {
+                gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+                vTextureCoord = aVertexTexCoord;
+            }
+        `,
+        fs:`
+            varying highp vec2 vTextureCoord;
+
+            uniform sampler2D uSampler;
+
+            void main() {
+                gl_FragColor = texture2D(uSampler, vTextureCoord);
+            }
+        `
+    })
+
+    /** @type {ProgramInfo} */
+    const programInfo = {
+        program: shaderProgram,
+        attribLocations: {
+            vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+            vertexUVCoords: gl.getAttribLocation(shaderProgram, "aVertexTexCoord"),
+        },
+        uniformLocations: {
+            projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
+            modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+            uSampler: gl.getUniformLocation(shaderProgram, "uSampler"),
+        },
+    };
+
+    return programInfo
+}
+
+/**
+ * @typedef {{
+ * position:WebGLBuffer,
+ * uvs:WebGLBuffer,
+ * indices:WebGLBuffer,
+ * texture:WebGLTexture,
+ * }} Buffer
+ * */
+/**
+ * 
+ * @param {GL} gl 
+ * @returns {Buffer}
+ */
+function makeBuffers(gl){
+    /** @type {Buffer} */
+    const buffers={}
+
+    buffers.position=gl.createBuffer()
+    gl.bindBuffer(GL.ARRAY_BUFFER, buffers.position)
+    const positions=new Float32Array([
+        // Front face (4 edges)
+        -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0,
+
+        // Back face (4 edges)
+        -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0,
+
+        // Top face (4 edges)
+        -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0,
+
+        // Bottom face (4 edges)
+        -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0,
+
+        // Right face (4 edges)
+        1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0,
+
+        // Left face (4 edges)
+        -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0,
+    ])
+    gl.bufferData(GL.ARRAY_BUFFER, positions, GL.STATIC_DRAW)
+
+    buffers.uvs=gl.createBuffer()
+    gl.bindBuffer(GL.ARRAY_BUFFER, buffers.uvs)
+    const uvs=new Float32Array([
+        // Front
+        0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+        // Back
+        0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+        // Top
+        0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+        // Bottom
+        0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+        // Right
+        0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+        // Left
+        0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+    ])
+    gl.bufferData(GL.ARRAY_BUFFER, uvs, GL.STATIC_DRAW)
+
+    buffers.indices=gl.createBuffer()
+    gl.bindBuffer(GL.ELEMENT_ARRAY_BUFFER,buffers.indices)
+    const indices=new Uint16Array([
+        0,  1,  2,      0,  2,  3,    // front
+        4,  5,  6,      4,  6,  7,    // back
+        8,  9,  10,     8,  10, 11,   // top
+        12, 13, 14,     12, 14, 15,   // bottom
+        16, 17, 18,     16, 18, 19,   // right
+        20, 21, 22,     20, 22, 23,   // left
+    ])
+    gl.bufferData(GL.ELEMENT_ARRAY_BUFFER,indices,GL.STATIC_DRAW)
+
+    buffers.texture=gl.createTexture()
+    gl.bindTexture(GL.TEXTURE_2D, buffers.texture)
+    const width=4;const height=3;
+    const imgData=new Uint8Array([
+        255,0,0,255,
+          0,0,0,255,
+        255,0,0,255,
+        255,0,0,255,
+        
+        255,0,0,255,
+          0,0,0,255,
+        255,0,0,255,
+        255,0,0,255,
+
+        255,0,0,255,
+        255,0,0,255,
+          0,0,0,255,
+        255,0,0,255,
+    ])
+
+    // Flip image pixels into the bottom-to-top order that WebGL expects.
+    // must be called BEFORE image data is uploaded!
+    gl.pixelStorei(GL.UNPACK_FLIP_Y_WEBGL, true);
+
+    gl.texImage2D(
+        GL.TEXTURE_2D,
+        0,
+        GL.RGBA,
+        width,height,
+        0,
+        GL.RGBA,
+        GL.UNSIGNED_BYTE,
+        imgData
+    )
+
+    // set these parameters on the bound texture (anytime between creation and usage)
+    gl.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.CLAMP_TO_EDGE)
+    gl.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, GL.CLAMP_TO_EDGE)
+    gl.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.NEAREST)
+    gl.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.NEAREST)
+
+    return buffers
+}
+
 /** @typedef {{buffers:Buffer,programInfo:ProgramInfo}} Scene */
 
 /**
@@ -107,11 +284,11 @@ function drawScene(gl,scene){
         0,
     )
 
-    // bind vertex data: colors
-    gl.bindBuffer(GL.ARRAY_BUFFER,buffers.color)
+    // bind vertex data: uv coords
+    gl.bindBuffer(GL.ARRAY_BUFFER,buffers.uvs)
     gl.vertexAttribPointer(
-        programInfo.attribLocations.vertexColor,
-        4,
+        programInfo.attribLocations.vertexUVCoords,
+        2,
         GL.FLOAT,
         false,
         0,
@@ -178,12 +355,16 @@ function drawScene(gl,scene){
         gl.clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT)
 
         gl.bindBuffer(GL.ARRAY_BUFFER,buffers.position)
-        gl.bindBuffer(GL.ARRAY_BUFFER,buffers.color)
+        gl.bindBuffer(GL.ARRAY_BUFFER,buffers.uvs)
         gl.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, buffers.indices);
-
+        
+        // prepare draw: bind texture
+        gl.activeTexture(gl.TEXTURE0)
+        gl.bindTexture(gl.TEXTURE_2D, buffers.texture)
+        gl.uniform1i(programInfo.uniformLocations.uSampler,0)
         // prepare draw: enable vertex data 
         gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition)
-        gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor)
+        gl.enableVertexAttribArray(programInfo.attribLocations.vertexUVCoords)
         // prepare draw: activate shader
         gl.useProgram(programInfo.program)
 
@@ -193,133 +374,6 @@ function drawScene(gl,scene){
         requestAnimationFrame(draw)
     }
     draw()
-}
-
-/**
- * @typedef {{
- * position:WebGLBuffer,
- * color:WebGLBuffer,
- * indices:WebGLBuffer,
- * }} Buffer
- * */
-/**
- * 
- * @param {GL} gl 
- * @returns {Buffer}
- */
-function makeBuffers(gl){
-    /** @type {Buffer} */
-    const buffers={}
-
-    buffers.position=gl.createBuffer()
-    gl.bindBuffer(GL.ARRAY_BUFFER, buffers.position)
-    const positions=new Float32Array([
-        // Front face (4 edges)
-        -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0,
-
-        // Back face (4 edges)
-        -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0,
-
-        // Top face (4 edges)
-        -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0,
-
-        // Bottom face (4 edges)
-        -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0,
-
-        // Right face (4 edges)
-        1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0,
-
-        // Left face (4 edges)
-        -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0,
-    ])
-    gl.bufferData(GL.ARRAY_BUFFER, positions, GL.STATIC_DRAW)
-
-    buffers.color=gl.createBuffer()
-    gl.bindBuffer(GL.ARRAY_BUFFER, buffers.color)
-    const colors=new Float32Array([
-        // repeat color once for each vertex per edge
-        Array(4).fill([1.0, 1.0, 1.0, 1.0]), // Front face: white
-        Array(4).fill([1.0, 0.0, 0.0, 1.0]), // Back face: red
-        Array(4).fill([0.0, 1.0, 0.0, 1.0]), // Top face: green
-        Array(4).fill([0.0, 0.0, 1.0, 1.0]), // Bottom face: blue
-        Array(4).fill([1.0, 1.0, 0.0, 1.0]), // Right face: yellow
-        Array(4).fill([1.0, 0.0, 1.0, 1.0]), // Left face: purple
-        // then flatten
-    ].flat(3))
-    gl.bufferData(GL.ARRAY_BUFFER, colors, GL.STATIC_DRAW)
-
-    buffers.indices=gl.createBuffer()
-    gl.bindBuffer(GL.ELEMENT_ARRAY_BUFFER,buffers.indices)
-    const indices=new Uint16Array([
-        0,  1,  2,      0,  2,  3,    // front
-        4,  5,  6,      4,  6,  7,    // back
-        8,  9,  10,     8,  10, 11,   // top
-        12, 13, 14,     12, 14, 15,   // bottom
-        16, 17, 18,     16, 18, 19,   // right
-        20, 21, 22,     20, 22, 23,   // left
-    ])
-    gl.bufferData(GL.ELEMENT_ARRAY_BUFFER,indices,GL.STATIC_DRAW)
-    
-    return buffers
-}
-
-/**
- * @typedef {{
-*     program: WebGLProgram,
-*     attribLocations: {
-*         vertexPosition: GLint,
-*         vertexColor:GLint,
-*     },
-*     uniformLocations: {
-*         projectionMatrix: WebGLUniformLocation,
-*         modelViewMatrix: WebGLUniformLocation,
-*     }
-* }} ProgramInfo
-* */
-/**
- * 
- * @param {WebGL2RenderingContext} gl
- * @returns {ProgramInfo}
- */
-function makeProgram(gl){
-    const shaderProgram=createShaderProgram(gl,{
-        vs:`
-            attribute vec4 aVertexPosition;
-            attribute vec4 aVertexColor;
-
-            uniform mat4 uModelViewMatrix;
-            uniform mat4 uProjectionMatrix;
-
-            varying lowp vec4 vColor;
-
-            void main() {
-                gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-                vColor = aVertexColor;
-            }
-        `,
-        fs:`
-            varying lowp vec4 vColor;
-
-            void main() {
-                gl_FragColor = vColor;
-            }
-        `
-    })
-
-    /** @type {ProgramInfo} */
-    const programInfo = {
-        program: shaderProgram,
-        attribLocations: {
-            vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
-            vertexColor: gl.getAttribLocation(shaderProgram, "aVertexColor"),
-        },
-        uniformLocations: {
-            projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
-            modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
-        },
-    };
-
-    return programInfo
 }
 
 async function main(){
