aa39f9a28cddbd760a832a9807a291e571bf9afe
Patrick Hennig <patrick_hennig@fastmail.com>
2025-04-30 16:18:52 +0200

    switch from flask to fastapi

--- server.py
+++ server.py
@@ -4,6 +4,11 @@ import argparse
 from pathlib import Path
 import git
 import typing as tp
+import uvicorn
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import PlainTextResponse
+from fastapi.staticfiles import StaticFiles
+from asgiref.wsgi import WsgiToAsgi
 
 parser=argparse.ArgumentParser(description="")
 parser.add_argument("--port","-p",type=int,default=8000,help=f"Port to serve on. defaults to None.")
@@ -11,33 +16,19 @@ args=parser.parse_args()
 
 PORT=args.port
 
-from flask import Flask, send_file, send_from_directory, request
+app = FastAPI()
 
-# no need to specify port or manually start flask
-app = Flask(__name__)
-
-# -- actually implement routing logic below
-
-@app.route("/")
-@app.route("/index.html")
-def sendindex():
-    return serve_static_file("index.html")
-
-@app.route('/src/<path:requested_path>')
-def serve_script(requested_path:str):
-    return serve_static_file(str(Path("src")/requested_path))
-
-@app.route('/css/<path:requested_path>')
-def serve_style(requested_path:str):
-    return serve_static_file(str(Path("css")/requested_path))
-
-def serve_static_file(path:str):
-    return send_from_directory("static",path)
+app.mount(
+    "/",
+    # html=True -> serve index.html on path=="/"
+    StaticFiles(directory="static", html=True),
+    name="static",
+)
 
 # target for webhook that triggers code pull on commit.
 # from https://medium.com/@aadibajpai/deploying-to-pythonanywhere-via-github-6f967956e664
-@app.route("/update_server", methods=["POST"])
-def update_server():
+@app.post("/update_server")
+def update_server(request:Request):
     if request.method=="POST" and git is not None:
         repo=git.Repo("./waggle")
         origin=repo.remotes.origin
@@ -49,4 +40,6 @@ def update_server():
 
 # implement routes regardless, but only start server here if this is the main entry point
 if __name__ == '__main__':
-    app.run(port=PORT)
+    uvicorn.run(app, port=PORT)
+else:
+    application = WsgiToAsgi(app)
